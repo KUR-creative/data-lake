8b0bde637bc593250ae86bf29706e2168508fb03
kur <rhdnfka94@gmail.com>
2019-11-20 11:12:44 +0900

    Add deps for jdbc and sql dsl

--- project.clj
+++ project.clj
@@ -3,7 +3,11 @@
   :url "http://example.com/FIXME"
   :license {:name "EPL-2.0 OR GPL-2.0-or-later WITH Classpath-exception-2.0"
             :url "https://www.eclipse.org/legal/epl-2.0/"}
-  :dependencies [[org.clojure/clojure "1.10.0"]]
+  :dependencies [[org.clojure/clojure "1.10.0"]
+                 [org.clojure/java.jdbc "0.7.9"]
+                 [org.xerial/sqlite-jdbc "3.28.0"]
+                 [honeysql "0.9.5"]
+                 ]
   :main ^:skip-aot data-lake.core
   :target-path "target/%s"
   :profiles {:uberjar {:aot :all}})
