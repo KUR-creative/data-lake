075f8e1c84291c858c11edb8e5f604db4386a10e
kur <rhdnfka94@gmail.com>
2019-11-20 11:13:22 +0900

    Add noexcept db do func

--- src/data_lake/core.clj
+++ src/data_lake/core.clj
@@ -2,9 +2,20 @@
 ;map <F6> :wa<CR>:!lein test<CR>
 ;map <F7> :wa<CR>:!lein run<CR>
 (ns data-lake.core
+  (:require [clojure.java.jdbc :as jdbc]
+            [honeysql.core :as sql]
+            [honeysql.helpers :as h]
+            )
   (:gen-class))
 
 (defn -main
   "I don't do a whole lot ... yet."
   [& args]
   (println "Hello, World!"))
+
+(defn db-do-noexcept
+  "Do cmd on db"
+  [db cmd]
+  (try (jdbc/db-do-commands db cmd) 
+       (catch Exception e
+              (println (.getMessage e)))));TODO: logging
