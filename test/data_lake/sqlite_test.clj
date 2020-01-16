a6ce20ffb1218b99eeda4c8050f77bb1985c8099
kur <rhdnfka94@gmail.com>
2020-01-16 16:08:13 +0900

    Test: Add test for create - insert - read sqlite db

--- test/data_lake/sqlite_test.clj
+++ test/data_lake/sqlite_test.clj
@@ -4,6 +4,8 @@
             [clojure.java.io :as io]
             [orchestra.spec.test :as st]
             [clojure.edn :as edn]
+            [honeysql.core :as sql]
+            [honeysql.helpers :as h]
             [data-lake.task.sqlite :refer :all]))
 
 
@@ -15,6 +17,9 @@
     [:path      :TEXT    "NOT NULL"]
     [:extension :TEXT    "NOT NULL"]
     [:size      :INTEGER "NOT NULL"]]
+   :ref_test
+   [[:ref :BLOB "NOT NULL"
+     :REFERENCES "file(guid)"]]
    :annotation
    [[:input  :BLOB "NOT NULL"] ;guid
     [:output :BLOB "NOT NULL"] ;guid
@@ -45,10 +50,36 @@
                  db))
           (.delete file)))))
 
+;#uuid "6261bc97-e39c-4c2c-9ca2-36c858ce5ced"
+(deftest create!-io-test
+  (testing "insert and get"
+    (let [path "./test/fixture/test4.db" 
+          file (io/as-file path)
+          db   (create! path schema-map)
+          row  #(identity {:guid (java.util.UUID/randomUUID)
+                           :path "test"
+                           :extension "png"
+                           :size 123})
+          rows (vec (repeatedly 3 row))
+          _    (doseq [row rows] 
+                 (jdbc/insert! db :file row))
+          out  (jdbc/query db (-> (h/select :guid)
+                                  (h/from :file)
+                                  sql/format))] 
+      (do (is (= (into #{} (map :guid rows))
+                 (into #{} (map #(java.util.UUID/fromString (:guid %)) out))
+                 #_(into #() (map :guid out))))
+          (.delete file)))))
+
+
+;actual: (not (= 
+;#{#uuid "c59c9852-9b73-40b8-aac6-b9d46101e3af" #uuid "6cbe2a6f-43e4-4a67-868d-b6740d215104" #uuid "6261bc97-e39c-4c2c-9ca2-36c858ce5ced"} 
+;#{{:guid "6261bc97-e39c-4c2c-9ca2-36c858ce5ced"} {:guid "6cbe2a6f-43e4-4a67-868d-b6740d215104"} {:guid "c59c9852-9b73-40b8-aac6-b9d46101e3af"}}))
+
 (def edn-path "./DB/sqlite/szmc_schema_0.1.0.edn")
 (deftest szmc-sqlite-v0.1.0-schema-test
   (testing "create szmc v0.1.0 sqlite db from edn"
-    (let [path "./test/fixture/test4.db" 
+    (let [path "./test/fixture/test5.db" 
           file (io/as-file path)
           m    (edn/read-string (slurp edn-path))
           _    (create! path (:schema m))]
