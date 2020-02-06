592279bdf6981d594ae494a1b24d1183e0435309
kur <rhdnfka94@gmail.com>
2020-02-06 14:11:11 +0900

    Style: Use exceptional style for jdbc/insertclear;git df

--- src/data_lake/core/add1.clj
+++ src/data_lake/core/add1.clj
@@ -7,10 +7,11 @@
 (defn add!
   "Add 1 image into db"
   [db img-path]
-  (jdbc/insert! db 
-                :file
-                {:guid      (java.util.UUID/randomUUID)
-                 :path      img-path
-                 :extension (fu/ext-without-dot img-path)
-                 :size      (.length (io/file img-path))
-                 }))
+  (jdbc/insert! 
+    db 
+    :file
+    {:guid      (java.util.UUID/randomUUID) 
+     :path      img-path
+     :extension (fu/ext-without-dot img-path)
+     :size      (.length (io/file img-path))})
+  )
