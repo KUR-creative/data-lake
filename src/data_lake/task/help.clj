729903948ab6828a0648c44264fc92ba166b8328
kur <rhdnfka94@gmail.com>
2020-03-19 17:21:25 +0900

    Doc: Change some old docs contents and style

--- src/data_lake/task/help.clj
+++ src/data_lake/task/help.clj
@@ -7,7 +7,7 @@
  lein run init    Initiate system if not initiated.
  
  lein run sqlite new <name.db-path> <schema.edn-path>
- Create new empty sqlite db in <name.db-path> with schema from <schema.edn-path>
+   Create new empty sqlite db in <name.db-path> with schema from <schema.edn-path>
  ")
 ;; TODO: autogenerate above...
 
